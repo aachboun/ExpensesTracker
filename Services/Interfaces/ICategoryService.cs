efc0f3444a9fac242ce7fc76be8955a348681700
aachboun <r.aachboun003@gmail.com>
2026-01-07 17:10:34 +0100

    Adding README.md

--- Services/Interfaces/ICategoryService.cs
+++ Services/Interfaces/ICategoryService.cs
@@ -1,6 +1,14 @@
-﻿namespace ExpensesTracker.Services.Interfaces
+﻿using ExpensesTracker.DTOs.Category;
+
+namespace ExpensesTracker.Services.Interfaces
 {
     public interface ICategoryService
     {
+
+        Task<int> CreateAsync(CreateCategoryDto dto , string UserId);
+        Task UpdateAsync( int id , UpdateCategoryDto dto , string UserId);
+        Task DeleteAsync(int id , string UserId);
+        Task<List<ReadCategoryDto>> GetAllAsync(string UserId);
+        Task<ReadCategoryDto> GetByIdAsync(int id , string UserId);
     }
 }
