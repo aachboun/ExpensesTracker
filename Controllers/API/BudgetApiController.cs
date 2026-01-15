6bc55519c2c9af9c3d7fb18c5ca0bf956910c86c
aachboun <r.aachboun003@gmail.com>
2026-01-15 17:57:27 +0100

    Initilize the BudgetApiController

---
+++ Controllers/API/BudgetApiController.cs
@@ -0,0 +1,26 @@
+﻿using ExpensesTracker.Models;
+using ExpensesTracker.Services;
+using ExpensesTracker.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ExpensesTracker.Controllers.API
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BudgetApiController : ControllerBase
+    {
+        private readonly IBudgetService _budgetService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public BudgetApiController(IBudgetService budgetService, UserManager<ApplicationUser> userManager)
+        {
+            _budgetService = budgetService;
+            _userManager = userManager;
+        }
+
+    }
+}
